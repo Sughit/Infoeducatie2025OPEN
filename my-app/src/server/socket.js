39f93d7c32b3df433f4255baaa53e4fad90c471f
Sughit <andreisorici2007@gmail.com>
2025-07-30 13:01:22 +0300

    .

--- my-app/src/server/socket.js
+++ my-app/src/server/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://46bd291c-eb21-4480-8a00-06dc2d3ed965-00-3an6l4em0odn9.janeway.replit.dev";
+const url = process.env.REACT_APP_SOCKET_URL;
 
-export const socket = io(SOCKET_URL, {
+export const socket = io(url, {
   autoConnect: false, 
 });
